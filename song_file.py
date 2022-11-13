1247995dccb8f97deb38641a87587ddc0b56c9ff
Cowlife <56398689+Cowlife@users.noreply.github.com>
2022-11-13 00:58:26 +0000

    Creating and automatization of chart track. Next Step: Convert it into an usable object worthy of mutation.

--- song_file.py
+++ song_file.py
@@ -19,13 +19,13 @@ def load(map):
                 print(key_pressers)
                 key_pressers.move_ip(650, 350)
             elif data[y][x] == '=':
-                key_pressers = pygame.Rect(x * 100, y * 100, 50, 25)
-                key_pressers2 = pygame.Rect(x * 100 - 75, y * 100, 50, 25)
+                key_pressers = pygame.Rect(x * 100 + 50, y * 100, 50, 25)
+                key_pressers2 = pygame.Rect(x * 100 - 50, y * 100, 50, 25)
                 rects.append(key_pressers)
                 rects.append(key_pressers2)
                 print(key_pressers)
-                key_pressers.move_ip(650, 350)
-                key_pressers2.move_ip(650, 350)
+                key_pressers.move_ip(550, 350)
+                key_pressers2.move_ip(550, 350)
     return rects
 
 
@@ -37,7 +37,7 @@ def song(screen):
     # Creating the sprites and groups
 
     # now we will create a map by making a txt file
-    map_rect = load("music3")
+    map_rect = load("rythm test")
 
     while True:
 
@@ -62,8 +62,10 @@ def song(screen):
             pygame.draw.rect(screen, (200, 0, 0), rect)
             rect.x -= 5
             for key in keys:
-                if key.rect.colliderect(rect) and not key.handled:
+                if key.rect.colliderect(rect) and key.handled:
                     map_rect.remove(rect)
+                    sound_effecting = pygame.mixer.Sound("sound_effects/attack sound effect.wav")
+                    pygame.mixer.Sound.play(sound_effecting)
                     key.handled = True
                     break
 
